9ac22ae4d0dba50319e456919d0dd5b3ffbc0b7e
milkmidi <milkmidi@gmail.com>
2018-04-19 17:22:30 +0800

    add GetUserMedia Example

--- src/js/component/GetUserMedia/GetUserMedia.jsx
+++ src/js/component/GetUserMedia/GetUserMedia.jsx
@@ -1,15 +1,47 @@
 /* eslint no-return-assign:0 */
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 // import PropTypes from 'prop-types';
-// import $script from 'scriptjs';
+import $script from 'scriptjs';
 import * as PIXI from 'pixi.js';
 import './GetUserMedia.styl';
+// import styled from 'styled-components';
 
 
-class GetUserMedia extends Component {
+class GetUserMedia extends PureComponent {
   canvas:HTMLCanvasElement;
   app:PIXI.Application;
 
+  state = {
+    showCaptureButton: false,
+    startCapture: false,
+  }
+  startCapture = () => {
+    this.setState({
+      startCapture: !this.state.startCapture,
+    }, () => {
+      if (this.state.startCapture) {
+        console.log('start');
+        this.capturer = new CCapture({
+          verbose: false,
+          display: true,
+          framerate: 12,
+          format: 'webm',
+          workersPath: './js/',
+          timeLimit: 10,
+          frameLimit: 0,
+          autoSaveTime: 0,
+          onProgress(p) { console.log(p); },
+        });
+        this.capturer.start();
+      } else {
+        console.log('stop');
+        this.capturer.stop();
+        this.capturer.save((blob) => {
+          console.log(blob);
+        });
+      }
+    });
+  }
   getUserMedia():Promise<void> {
     const constraints = {
       audio: false,
@@ -18,22 +50,29 @@ class GetUserMedia extends Component {
     return navigator.mediaDevices.getUserMedia(constraints);
   }
   initPIXI() {
-    const app = new PIXI.Application(800, 600, { transparent: true, view: this.canvas });
+    const width = this.video.clientWidth;
+    const height = this.video.clientHeight;
+    const app = new PIXI.Application(
+      width, height,
+      {
+        transparent: true,
+        view: this.canvas,
+        antialias: true,
+        autoStart: false,
+      },
+    );
     this.app = app;
 
-    const texture = PIXI.Texture.fromVideo(document.querySelector('video'));
-
-    // create a new Sprite using the video texture (yes it's that easy)
+    const texture = PIXI.Texture.fromVideo(this.video);
     const videoSprite = new PIXI.Sprite(texture);
-    videoSprite.filters = [new PIXI.filters.BlurFilter(16, 16)];
-
-    // Stetch the fullscreen
+    this.videoSprite = videoSprite;
+    videoSprite.filters = [new PIXI.filters.BlurFilter(4, 4)];
     videoSprite.width = app.screen.width;
     videoSprite.height = app.screen.height;
-
     app.stage.addChild(videoSprite);
 
     const graphics = new PIXI.Graphics();
+    this.graphics = graphics;
 
     // set a fill and line style
     graphics.beginFill(0xFF3300);
@@ -46,15 +85,15 @@ class GetUserMedia extends Component {
     graphics.lineTo(50, 50);
     graphics.endFill();
     app.stage.addChild(graphics);
-
-    const mask = new PIXI.Graphics();
-    mask.beginFill(0);
-    mask.drawRect(0, 0, 400, 600);
-    videoSprite.mask = mask;
-
-    app.ticker.add(() => {
-      graphics.x = Math.sin(new Date().getTime() / 500) * 100 + 400;
-    });
+    this.startRender();
+  }
+  startRender =() => {
+    this.graphics.x = Math.sin(new Date().getTime() / 500) * 100;
+    this.app.render();
+    if (this.capturer && this.state.startCapture) {
+      this.capturer.capture(this.canvas);
+    }
+    requestAnimationFrame(this.startRender);
   }
   async componentDidMount() {
     try {
@@ -67,6 +106,12 @@ class GetUserMedia extends Component {
     } catch (error) {
       alert(error);
     }
+
+    $script('https://www.clicktorelease.com/code/conway3d_ccapture/js/CCapture.all.min.js', () => {
+      this.setState({
+        showCaptureButton: true,
+      });
+    });
   }
   componentWillUnmount() {
     if (this.app) {
@@ -76,10 +121,16 @@ class GetUserMedia extends Component {
     }
   }
   render() {
+    const { showCaptureButton, startCapture } = this.state;
     return (
       <section className="get-user-media-root">
         <video playsInline autoPlay ref={dom => this.video = dom}></video>
         <canvas ref={dom => this.canvas = dom} />
+        {
+          showCaptureButton
+            ? <button onClick={this.startCapture}>{startCapture ? 'Stop' : 'Capture'}</button>
+            : null
+        }
       </section>
     );
   }
